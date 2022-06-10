47f8a4241134421e160e5b2293594fb01844387b
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-06-10 18:51:18 +0200

    Comments on settings_screen.

--- lib/screens/navigation_screens/settings_screen.dart
+++ lib/screens/navigation_screens/settings_screen.dart
@@ -6,6 +6,9 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+// Pantalla de ajustes. Precisa extender de StatefulWidget para poder
+// redibujar en tiempo de ejecución (cuando se hace click en algún control)
+// la propia pantalla.
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
@@ -18,20 +21,28 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // Botón de selección del tema oscuro.
         SwitchListTile.adaptive(
+            // Utilizo clase "global" que hace uso de las SharedPreferences.
             value: Preferences.isDarkMode,
             title: const Text('Modo oscuro'),
             onChanged: (value) {
+              // Defino la propiedad dentro de la clase globalizada que hace uso
+              // de las SharedPreferences.
               Preferences.isDarkMode = value;
 
+              // Obtengo el ThemeProvider para así definirlo y notificar cambio
+              // al Main (para que redibuje toda la aplicación).
               final themeProvider =
                   Provider.of<ThemeProvider>(context, listen: false);
 
+              // Valoro el nuevo valor que toma el switch.
               if (value)
                 themeProvider.setDarkMode();
               else
                 themeProvider.setLightMode();
 
+              // Redibujo la pantalla local.
               setState(() {});
             })
       ],
