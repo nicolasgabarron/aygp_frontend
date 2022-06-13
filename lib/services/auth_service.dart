0b710ccdf1b0286a762ff66d1f0b993dca43cb55
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-06-13 19:41:18 +0200

    Split JWT response in login and change 'fechaNacimiento' field type to String.

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -8,7 +8,7 @@ class AuthService extends ChangeNotifier {
   // Propiedades.
   final String _baseUrl = 'localhost:9090';
 
-  Future<String?> createUser(String name, String surname, DateTime date,
+  Future<String?> createUser(String name, String surname, String date,
       String email, String username, String password) async {
     // JSON de SALIDA con los datos introducidos por el usuario.
     final Map<String, dynamic> registerData = {
@@ -62,10 +62,15 @@ class AuthService extends ChangeNotifier {
     if (response.statusCode == 200) {
       final Map<String, dynamic> recievedData = json.decode(response.body);
 
-      final String jwt = response.headers[
-          'set-cookie']!; // Si hace login, este header va a venir si o si.
+      final String? jwt = response.headers[
+          'set-cookie']; // Si hace login, este header va a venir si o si.
 
-      return jwt;
+      if (jwt != null) {
+        var splittedCookies = jwt.split(';');
+
+        // TODO: Hacer substring para quitar el "nicogbdev_jwt".
+        return splittedCookies[0];
+      }
     } else {
       return 'error'; // TODO: Cambiar por mensaje con más información (quizás el propio status code.)
     }
