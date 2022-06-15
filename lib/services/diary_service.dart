24c47ace639e553ab99c8cb98c81cb2839310b67
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-06-15 18:52:24 +0200

    Error handling in fetch data from REST.

--- lib/services/diary_service.dart
+++ lib/services/diary_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:aygp_frontend/models/diary_entry.dart';
+import 'package:aygp_frontend/services/notifications_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
@@ -34,31 +35,37 @@ class DiaryService extends ChangeNotifier {
     // Almaceno la petición.
     final response = await http.get(url, headers: headers);
 
-    // Recupero la lista de JSONs.
-    final List<dynamic> rawResponse = json.decode(response.body);
+    // Compruebo posibles errores en la respuesta.
+    if (response.statusCode == 200) {
+      // Recupero la lista de JSONs.
+      final List<dynamic> rawResponse = json.decode(response.body);
 
-    // Creo una lista de MAPAS (Esto es necesario ya que el parser de
-    // DiaryEntry (modelo) fromMap, necesita un Mapa, y Flutter no hace la conversión
-    // directa con el json.decode)
-    final List<Map<String, dynamic>> rawMapResponse = [];
+      // Creo una lista de MAPAS (Esto es necesario ya que el parser de
+      // DiaryEntry (modelo) fromMap, necesita un Mapa, y Flutter no hace la conversión
+      // directa con el json.decode)
+      final List<Map<String, dynamic>> rawMapResponse = [];
 
-    // Recorro cada elemento de la respuesta en BRUTO.
-    rawResponse.forEach((element) {
-      rawMapResponse.add(element);
-    });
+      // Recorro cada elemento de la respuesta en BRUTO.
+      rawResponse.forEach((element) {
+        rawMapResponse.add(element);
+      });
 
-    // Recorro cada MAPA (un Json) para transformarlo en objeto y añadirlo a la lista.
-    rawMapResponse.forEach(
-      (element) {
-        final tempDiaryEntry = DiaryEntry.fromMap(element);
-        this.diaryEntries.add(tempDiaryEntry);
-      },
-    );
+      // Recorro cada MAPA (un Json) para transformarlo en objeto y añadirlo a la lista.
+      rawMapResponse.forEach(
+        (element) {
+          final tempDiaryEntry = DiaryEntry.fromMap(element);
+          this.diaryEntries.add(tempDiaryEntry);
+        },
+      );
 
-    // NOTIFICO QUE SE HA DEJADO DE CARGAR.
-    this.isLoading = false;
-    notifyListeners();
+      // NOTIFICO QUE SE HA DEJADO DE CARGAR.
+      this.isLoading = false;
+      notifyListeners();
 
-    return this.diaryEntries;
+      return this.diaryEntries;
+    } else {
+      NotificationsService.showSnackbar(
+          'No se ha podido traer los datos del servidor.', true);
+    }
   }
 }
