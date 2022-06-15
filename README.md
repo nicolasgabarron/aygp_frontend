086683e9df9b905ef56603e343a4cbc7084d56f6
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-06-15 14:11:36 +0200

    Basic graphic controls on DiaryEditScreen.

--- README.md
+++ README.md
@@ -2,15 +2,12 @@
 
 Aplicación móvil para el proyecto Ayuda y Gestión Psicológica.
 
-## Getting Started
+## Futuras mejoras.
+  - Cuando se guarde un suceso clave, se guardará la ubicación, y luego en la ficha de edición, se mostrará dicha ubicación en un mapa (Apple Mapas o Google Maps).
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## A documentar...
+  - Providers.
+  - AuthProvider.
+  - SecureStorage (login, logout)
+  - CheckAuthScreen (FutureBuilder con Microtasks).
+  
--- lib/screens/navigation_screens/diary/diary_edit_screen.dart
+++ lib/screens/navigation_screens/diary/diary_edit_screen.dart
@@ -1,12 +1,62 @@
+import 'package:aygp_frontend/ui/input_decorations.dart';
 import 'package:flutter/material.dart';
 
 class DiaryEditScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Pantalla de edición de entrada de diario.'),
+      appBar: AppBar(title: Text('Entrada de Diario')),
+      body: SingleChildScrollView(child: _DiaryForm()),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () {
+          // TODO: Guardar o modificar entrada de diario.
+        },
       ),
+    );
+  }
+}
+
+class _DiaryForm extends StatelessWidget {
+  const _DiaryForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(),
+      width: double.infinity,
+      child: Form(
+          child: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+
+          // TITULO
+          TextFormField(
+            decoration: InputDecorations.authInputDecoration(
+                hintText: 'Introduzca el título que quiere dar a la entrada',
+                labelText: 'Título'),
+          ),
+
+          SizedBox(
+            height: 25,
+          ),
+
+          // CONTENIDO.
+          TextFormField(
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            decoration: InputDecorations.authInputDecoration(
+                hintText:
+                    'Escriba aquí el contenido de la entrada de diario...',
+                labelText: 'Contenido'),
+          )
+        ],
+      )),
     );
   }
 }
