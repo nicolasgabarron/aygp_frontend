8aec8770c1eef611e835239640fb68c283da4b1e
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-06-17 20:14:11 +0200

    Implement 'saveOrCreate' method on SucesoClaveService.

--- lib/services/suceso_clave_service.dart
+++ lib/services/suceso_clave_service.dart
@@ -70,4 +70,81 @@ class SucesoClaveService extends ChangeNotifier {
       return this.sucesosClave;
     }
   }
+
+  Future saveOrCreate(SucesoClave sucesoClave) async {
+    // Indico que procedo a guardar (o modificar) un Suceso clave.
+    isSaving = true;
+    notifyListeners();
+
+    // CREACIÓN DE SUCESO CLAVE.
+    if (sucesoClave.id == null) {
+      // Endpoint final
+      final url = Uri.http(_baseUrl, '/api/sclave/sucesos');
+
+      // Obtengo el JWT.
+      final userJwt = await secureStorage.read(key: 'nicogbdev_jwt');
+
+      // Headers (mando el JWT y el Content/Type).
+      final headers = {
+        HttpHeaders.cookieHeader: 'nicogbdev_jwt=$userJwt',
+        HttpHeaders.contentTypeHeader: 'application/json'
+      };
+
+      // Realizo la petición.
+      final response =
+          await http.post(url, headers: headers, body: sucesoClave.toJson());
+
+      // Si la petición se ha ejecutado correctamente...
+      if (response.statusCode == 201) {
+        // Notifico de que se ha realizado correctamente.
+        NotificationsService.showSnackbar(
+            'Entrada de diario creada satisfactoriamente.', false);
+
+        // Añado a la lista de entradas locales la devuelta por el servidor.
+        sucesosClave.add(SucesoClave.fromMap(json.decode(response.body)));
+        notifyListeners();
+      } else {
+        // Notifico de que NO se ha realizado correctamente.
+        NotificationsService.showSnackbar(
+            'Ha ocurrido algún error creando la entrada de diario.', true);
+      }
+    }
+    // MODIFICACIÓN DE SUCESO CLAVE
+    else {
+      // Endpoint final
+      final url = Uri.http(_baseUrl, '/api/sclave/sucesos/${sucesoClave.id}');
+
+      // Obtengo el JWT.
+      final userJwt = await secureStorage.read(key: 'nicogbdev_jwt');
+
+      // Headers (mando el JWT y el Content/Type).
+      final headers = {
+        HttpHeaders.cookieHeader: 'nicogbdev_jwt=$userJwt',
+        HttpHeaders.contentTypeHeader: 'application/json'
+      };
+
+      // Realizo la petición.
+      final response =
+          await http.patch(url, headers: headers, body: sucesoClave.toJson());
+
+      if (response.statusCode == 200) {
+        // Notifico de que se ha realizado correctamente.
+        NotificationsService.showSnackbar(
+            'Entrada de diario actualizada satisfactoriamente.', false);
+
+        // Actualizo la lista de la vista principal.
+        final findDiaryEntryIndex = this
+            .sucesosClave
+            .indexWhere((element) => element.id == sucesoClave.id);
+        this.sucesosClave[findDiaryEntryIndex] = sucesoClave;
+
+        isSaving = false;
+        notifyListeners();
+      } else {
+        // Notifico de que NO se ha realizado correctamente.
+        NotificationsService.showSnackbar(
+            'Ha ocurrido algún error actualizando la entrada de diario.', true);
+      }
+    }
+  }
 }
