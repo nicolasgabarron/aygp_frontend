36ebcf4a4b72d33eca47eb91ca25b54a38f62bc6
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-06-18 20:11:56 +0200

    Allow scroll when Keyboard appear.

--- lib/screens/navigation_screens/home_screen.dart
+++ lib/screens/navigation_screens/home_screen.dart
@@ -10,70 +10,84 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      height: MediaQuery.of(context).size.height -
+          kToolbarHeight -
+          kBottomNavigationBarHeight,
       decoration: BoxDecoration(
           gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
               colors: [Colors.blue[200]!, Colors.blue[600]!])),
-      child: Padding(
-        padding: EdgeInsets.all(15),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Text(
-            '¡Bienvenido,',
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          Text(
-            '${Preferences.name} ${Preferences.surname}!',
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Divider(
-            thickness: 3,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            'Frase del día:',
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          _QuoteWidget(),
-          SizedBox(
-            height: 15,
-          ),
-          Divider(
-            thickness: 3,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            'Anotaciones rápidas',
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
-          ),
-          Card(
-            color: Colors.white.withOpacity(0.75),
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: TextFormField(
-                onChanged: (value) => Preferences.quickNote = value,
-                initialValue: Preferences.quickNote,
-                maxLines: 20,
-                decoration: InputDecoration.collapsed(
-                    hintText: 'Introduce aquí alguna anotación rápida...'),
-              ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text(
+              '¡Bienvenido,',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
             ),
-          )
-        ]),
+            Text(
+              '${Preferences.name} ${Preferences.surname}!',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Divider(
+              thickness: 3,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Frase del día:',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _QuoteWidget(),
+            SizedBox(
+              height: 15,
+            ),
+            Divider(
+              thickness: 3,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Anotaciones rápidas',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25),
+            ),
+            Card(
+              color: Colors.white.withOpacity(0.75),
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  onChanged: (value) => Preferences.quickNote = value,
+                  initialValue: Preferences.quickNote,
+                  maxLines: 15,
+                  decoration: InputDecoration.collapsed(
+                      hintText: 'Introduce aquí alguna anotación rápida...'),
+                ),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
